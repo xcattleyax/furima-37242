4196255282d4c4fd5dcf48e91966283a066cc3d1
xcattleyax <novels.satellite@gmail.com>
2021-12-05 14:31:19 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  with_options presence: true, format: { with:/\A[ぁ-んァ-ン一-龥]/, message: '全角で入力してください'} do
+    validates :last_name
+    validates :first_name
+  end
+  with_options presence: true, format: { with:/\A[ァ-ヶ--]+\z/, message: '全角カタカナで入力してください'} do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+  validates :birthday, presence: true
 end
