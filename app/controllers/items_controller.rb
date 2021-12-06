99cbc58cd857d06aac41dc80eef7b76c051a26ab
xcattleyax <novels.satellite@gmail.com>
2021-12-06 15:09:20 +0900

    createアクションの定義、newアクションの未ログイン時の処理の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only:[:new]
+
   def index
   end
 
@@ -7,5 +9,17 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :info, :sales_status_id, :shipping_fee_id, :prefecture_id, :scheduled_delivery_id, :price, :image).merge(user_id: current_user.id) 
+  end
+
 end
