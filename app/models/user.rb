d1938ec7a7578df7429ff2ebbde9e95e1e7ba02a
xcattleyax <novels.satellite@gmail.com>
2021-12-06 11:31:48 +0900

    with_optionsにてpresence: trueをまとめた

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,18 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname, presence: true
-  with_options format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: '半角のアルファベット、数字の両方を使用してください' } do
-    validates :password
+  with_options presence: ture do
+    validates :nickname
+    with_options format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: '半角のアルファベット、数字の両方を使用してください' } do
+      validates :password
+    end
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥々ー]/, message: '全角で入力してください' } do
+      validates :last_name
+      validates :first_name
+    end
+    with_options format: { with: /\A[ァ-ヶ--]+\z/, message: '全角カタカナで入力してください' } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+    validates :birthday
   end
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]/, message: '全角で入力してください' } do
-    validates :last_name
-    validates :first_name
-  end
-  with_options presence: true, format: { with: /\A[ァ-ヶ--]+\z/, message: '全角カタカナで入力してください' } do
-    validates :last_name_kana
-    validates :first_name_kana
-  end
-  validates :birthday, presence: true
 end
