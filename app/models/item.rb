77b51f61211eefb300a3a83870ee8ca9fa1071c3
xcattleyax <novels.satellite@gmail.com>
2021-12-06 12:18:19 +0900

    ActiveHashを作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,10 @@ class Item < ApplicationRecord
 
   belongs_to :user
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :SalsesStatus
+  belongs_to :ShippingFee
+  belongs_to :Prefecture
+  belongs_to :ScheduledDelivery
 end
