a2fcc22b7c13418dc10f4bdb5237e768636bee3e
xcattleyax <novels.satellite@gmail.com>
2021-12-06 12:36:59 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,20 @@ class Item < ApplicationRecord
   belongs_to :ShippingFee
   belongs_to :Prefecture
   belongs_to :ScheduledDelivery
+
+  with_options presence: true do
+    validates :name
+    validates :info
+    with_options numericality: { other_than: 1 , message: "can't be blank"} do
+      validates :genre_id
+      validates :SalsesStatus
+      validates :ShippingFee
+      validates :Prefecture
+      validates :ScheduledDelivery
+    end
+    with_options numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9,999,999}, format:{ with: /^[0-9]*$/, message: '半角で入力してください' } do
+    validates :price
+    end
+  end
+  
 end
