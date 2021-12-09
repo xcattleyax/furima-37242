f9f28331e48070e69cbba1b89fd21c07c3d06338
xcattleyax <novels.satellite@gmail.com>
2021-12-09 16:05:30 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
       validates :last_name
       validates :first_name
     end
-    with_options format: { with: /\A[ァ-ヶ--]+\z/, message: '全角カタカナで入力してください' } do
+    with_options format: { with: /\A[ァ-ヶ-ー]+\z/, message: '全角カタカナで入力してください' } do
       validates :last_name_kana
       validates :first_name_kana
     end
